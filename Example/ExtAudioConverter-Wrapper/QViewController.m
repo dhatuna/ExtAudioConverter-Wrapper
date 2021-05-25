a25b5477fd62fc0f2b16c5b9d6c6903b3ab69588
JK <junq.jeon@gmail.com>
2021-05-25 09:48:57 +0900

    initial commit

--- Example/ExtAudioConverter-Wrapper/QViewController.m
+++ Example/ExtAudioConverter-Wrapper/QViewController.m
@@ -8,22 +8,61 @@
 
 #import "QViewController.h"
 
+#import <ExtAudioConverter_Wrapper/ExtAudioConverter.h>
+
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+
 @interface QViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *convertButton;
 
 @end
 
 @implementation QViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    self.statusLabel.text = nil;
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)convertButtonTapped:(id)sender {
+    [self convert];
+}
+
+- (void)convert {
+//    NSString *inputPath = [[NSBundle mainBundle] pathForResource:@"source" ofType:@"m4a"];
+//    NSURL *outputURL = [[[[NSFileManager defaultManager] URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] firstObject] URLByAppendingPathComponent:@"output.mp3"];
+//    NSString *outputPath = outputURL.path;
+//
+//    NSLog(@"Input Path: %@", inputPath);
+//    NSLog(@"Output Path: %@", outputPath);
+//
+//    ExtAudioConverter *converter = [[ExtAudioConverter alloc] init];
+//    converter.inputFilePath = inputPath;
+//    converter.outputFilePath = outputPath;
+//    converter.outputFormatID = kAudioFormatMPEGLayer3;
+//    if ([converter convert]) {
+//        self.statusLabel.text = @"It worked!";
+//        AVPlayer *player = [AVPlayer playerWithURL:outputURL];
+//        AVPlayerViewController *vc = [[AVPlayerViewController alloc] init];
+//        vc.player = player;
+//        [self presentViewController:vc animated:YES completion:^{
+//            [player play];
+//        }];
+//    }
+//    else {
+//        self.statusLabel.text = @"It failed!";
+//    }
+}
+
 
 @end
--- ExtAudioConverter-Wrapper.podspec
+++ ExtAudioConverter-Wrapper.podspec
@@ -8,29 +8,24 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ExtAudioConverter-Wrapper'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of ExtAudioConverter-Wrapper.'
+  s.version          = '1.0.1'
+  s.summary          = 'Integrate lame.framework dependency with ExtAudioConverter.'
 
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
+  s.description      = "ExtAudioConverter-Wrapper is a copy of ExtAudioConverter provided by lixing123"
 
-  s.description      = <<-DESC
-TODO: Add long description of the pod here.
-                       DESC
-
-  s.homepage         = 'https://github.com/jk-gna/ExtAudioConverter-Wrapper'
+  s.homepage         = 'https://github.com/dhatuna/ExtAudioConverter-Wrapper'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'jk-gna' => 'junq.jeon@gmail.com' }
-  s.source           = { :git => 'https://github.com/jk-gna/ExtAudioConverter-Wrapper.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/dhatuna/ExtAudioConverter-Wrapper.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
+  s.ios.deployment_target = '12.0'
 
   s.source_files = 'ExtAudioConverter-Wrapper/Classes/**/*'
+  
+    s.static_framework = true
+  s.dependency 'lame'
   
   # s.resource_bundles = {
   #   'ExtAudioConverter-Wrapper' => ['ExtAudioConverter-Wrapper/Assets/*.png']
